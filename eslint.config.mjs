92637fa1e09fc64b1376c5c4645600a53796130c
Vahe Barseghyan <vahebarseghyan9999@gmail.com>
2025-09-24 16:46:30 +0400

    generate lib

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,9 +5,11 @@ export default [
     ...nx.configs["flat/typescript"],
     ...nx.configs["flat/javascript"],
     {
-        ignores: [
-            "**/dist"
-        ]
+      "ignores": [
+        "**/dist",
+        "**/vite.config.*.timestamp*",
+        "**/vitest.config.*.timestamp*"
+      ]
     },
     {
         files: [
