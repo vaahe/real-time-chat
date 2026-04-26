226fb91ddee4fea2466e7d3213cd768f9b912beb
Vahe Barseghyan <94294219+vaahe@users.noreply.github.com>
2026-04-26 09:22:14 -0700

    Add ChatGateway to AppModule controllers

--- apps/api-gateway/src/app/modules/app.module.ts
+++ apps/api-gateway/src/app/modules/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AuthGateway } from '../gateways/auth.gateway';
 import { UserGateway } from '../gateways/user.gateway';
+import { ChatGateway } from '../gateways/chat.gateway';
 import { PaymentGateway } from '../gateways/payment.gateway';
 
 @Module({
   imports: [],
-  controllers: [AuthGateway, UserGateway, PaymentGateway],
+  controllers: [AuthGateway, UserGateway, PaymentGateway, ChatGateway],
 })
 export class AppModule { }
