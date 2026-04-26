f5754be917987562a4fdfb358a182ba3ca464861
Vahe Barseghyan <94294219+vaahe@users.noreply.github.com>
2026-04-26 09:21:46 -0700

    Implement ChatGateway with gRPC authentication

--- apps/api-gateway/src/app/gateways/chat.gateway.ts
+++ apps/api-gateway/src/app/gateways/chat.gateway.ts
@@ -1,1 +1,32 @@
+import { join } from "path";
+import { Observable } from 'rxjs';
 
+import type { ClientGrpc } from "@nestjs/microservices";
+import { Client, Transport } from "@nestjs/microservices";
+import { Controller, Post, Body, OnModuleInit } from "@nestjs/common";
+import { AUTH_PACKAGE_NAME, AuthServiceClient, type SignUpRequest, SignUpResponse } from '@vaahe/proto';
+
+
+@Controller('auth')
+export class ChatGateway implements OnModuleInit {
+    @Client({
+        transport: Transport.GRPC,
+        options: {
+            url: 'localhost:3001',
+            package: CHAT_PACKAGE_NAME,
+            protoPath: join(__dirname, '../../../libs/proto/src/protos/auth.proto')
+        },
+    })
+    private client!: ClientGrpc;
+
+    private authService!: AuthServiceClient;
+
+    onModuleInit() {
+        this.authService = this.client.getService<AuthServiceClient>('AuthService');
+    }
+
+    @Post('sign-up')
+    async signUp(@Body() body: SignUpRequest): Promise<Observable<SignUpResponse>> {
+        return this.authService.signUp(body);
+    }
+}
