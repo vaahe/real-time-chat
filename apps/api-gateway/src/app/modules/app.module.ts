ac3f07bfc39e8499de2db9238632433b7ca53614
Vahe Barseghyan <94294219+vaahe@users.noreply.github.com>
2026-04-26 09:19:21 -0700

    Add PaymentGateway to AppModule controllers

--- apps/api-gateway/src/app/modules/app.module.ts
+++ apps/api-gateway/src/app/modules/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AuthGateway } from '../gateways/auth.gateway';
 import { UserGateway } from '../gateways/user.gateway';
+import { PaymentGateway } from '../gateways/payment.gateway';
 
 @Module({
   imports: [],
-  controllers: [AuthGateway, UserGateway],
+  controllers: [AuthGateway, UserGateway, PaymentGateway],
 })
 export class AppModule { }
